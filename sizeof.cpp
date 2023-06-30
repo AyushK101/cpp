4071b4a56b3ace5c2ec0f5ce709e77d1a44b82f2
ayush kumar <ayushboult318@gmail.com>
2023-06-30 16:57:05 +0530

    suffix intro in vale of sizeof

--- sizeof.cpp
+++ sizeof.cpp
@@ -7,5 +7,8 @@ int main()
     cout<<sizeof(char)<<"\n";
     cout<<sizeof(float)<<"\n";
     cout<<sizeof(long double)<<"\n";
+    cout<<sizeof(10LL)<<"\n";//when suffix an int value with LL it become long long 
+    cout<<sizeof(10.5)<<"\n";//double 
+    cout<<sizeof(10.5f); //become float
     return 0;
 }
