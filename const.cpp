2cc7fb81d6c7d1260b678971b927fa6db75f807e
ayush kumar <ayushboult318@gmail.com>
2023-06-30 18:49:17 +0530

    const example

---
+++ const.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+const double PI=3.14;
+int main()
+{
+    int r;
+    cout<<"enter radius\n";
+    cin>>r;
+    cout<<"area of circle is "<<(PI*r*r);
+    return 0;
+
+}
+
+
+
+
