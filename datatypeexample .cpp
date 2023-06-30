3313abf8516179dcfaab6bdf050bdeac8e10e8c4
ayush kumar <ayushboult318@gmail.com>
2023-06-30 16:31:10 +0530

    datatype example

---
+++ datatypeexample .cpp
@@ -0,0 +1,17 @@
+/*example of data type */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age=19;
+    string name="ayush";
+    char gender='M';
+    bool married=false;
+    double weight=53;
+    cout<<name<<"\n";
+    cout<<age<<"\n";
+    cout<<gender<<"\n";
+    cout<<married<<"\n";
+    cout<<weight;
+    return 0;
+}
