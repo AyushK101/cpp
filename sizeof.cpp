23d3bb0a6cd0bda70381ac1553e9266d9a0713cb
ayush kumar <ayushboult318@gmail.com>
2023-06-30 16:45:03 +0530

    sizeof example

---
+++ sizeof.cpp
@@ -0,0 +1,11 @@
+/*sizeof() operator example*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<sizeof(int)<<"\n";
+    cout<<sizeof(char)<<"\n";
+    cout<<sizeof(float)<<"\n";
+    cout<<sizeof(long double)<<"\n";
+    return 0;
+}
