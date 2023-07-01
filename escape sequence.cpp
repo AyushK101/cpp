dcb576c9ff9f16da5319cd64c28304ac334904a1
ayush kumar <ayushboult318@gmail.com>
2023-07-01 21:36:16 +0530

    escape sequence example

---
+++ escape sequence.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"welcome to \"gfg\""<<"\n"<<"welcome to \\ gfg"<<endl;
+    string s="C:\\abc\\xyz.txt";
+    cout<<s<<endl;
+    return 0;
+
+
+}
