080ef5fe12a030bdb430b340540587662bea5103
ayush kumar <ayushboult318@gmail.com>
2023-07-03 15:45:59 +0530

    int base flags

---
+++ oct/hex/dec.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=26;
+    cout<<std::hex;
+    cout<<x<<endl;
+    cout<<std::oct;
+    cout<<x<<endl;
+    cout<<std::dec;
+    cout<<x;
+    return 0;
+}
