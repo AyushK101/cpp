168bf74b6b76672ffdd52f74639843997cc4bf41
ayush kumar <ayushboult318@gmail.com>
2023-07-03 15:41:13 +0530

    int boolalpha io manupulation

---
+++ io manupulation.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // bool values
+    bool x=true;
+    cout<<x<<"\n";
+    cout<<std::boolalpha;
+    cout<<x<<"\n";
+    cout<<std::noboolalpha;
+    cout<<x;
+    
+    // integer values
+    
+    
+}
