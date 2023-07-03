ca07ffe17f4362e17c084e1600e1320c4fb8b422
ayush kumar <ayushboult318@gmail.com>
2023-07-03 16:10:21 +0530

    flot io manupulation

---
+++ floatingmanupulation.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    double x=12.3456;
+    cout<<std::setprecision(4);
+    cout<<x<<endl;
+    cout<<std::showpoint<<x;
+    return 0;
+
+    
+    
+    
+    }
+    
