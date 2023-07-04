c7bbcc891f1e0505df8e2c1504525db3f69970b2
ayush kumar <ayushboult318@gmail.com>
2023-07-04 06:02:50 +0530

    bitwise example

---
+++ bitwise.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=2,y=4;
+    cout<<(x&y)<<" "
+    <<(x|y)<<" "
+    <<(x^y)<<" "
+    <<(x<<y)<<" "
+    <<(x>>y)<<" "
+    <<(~x);
+    return 0;
+    
+}
