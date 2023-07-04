5eaa47d1227c1f14107c600c17a84c165fa7cf41
ayush kumar <ayushboult318@gmail.com>
2023-07-04 13:56:56 +0530

    if else user login age example example

---
+++ if-else.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cin>>age;
+    if(age<100)
+    {
+        cout<<"valid";
+    }
+    
+    else
+    {
+        cout<<"invalid";
+    }
+
+}
